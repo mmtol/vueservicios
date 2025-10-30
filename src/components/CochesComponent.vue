fca97095fb05fe526f3755d9cdddac3461b9c593
mmtol <mmigueltolosana@gmail.com>
2025-10-30 11:51:10 +0100

    coches

---
+++ src/components/CochesComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>Coches</h1>
+        <div v-for="coche in coches" :key="coche">
+            <h2>{{coche.marca}}</h2>
+            <img :src="coche.imagen"/>
+            <h3>{{coche.modelo}}</h3>
+            <h3>{{coche.conductor}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    //las variables pra que sean accesibles desde cualquier forma se declaran aqui
+    let url = "https://apicochespaco.azurewebsites.net/";
+
+    export default 
+    {
+        name:"CochesComponent",
+        data()
+        {
+            return{
+                coches:[]
+            }
+        },
+        methods:
+        {
+            cargarCoches()
+            {
+                var endPoint = "webresources/coches";
+                axios.get(url+endPoint).then(response =>
+                    {
+                        this.coches = response.data;
+                    }
+                )
+            }
+        },
+        mounted()
+        {
+            this.cargarCoches();
+        }
+    }
+</script>
+
+<style>
+    @import './../assets/css/estilos.css';
+</style>
